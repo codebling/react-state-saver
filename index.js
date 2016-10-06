18310e658fca8d22134593817eb147f748ff465b
Code Bling <codeblingx@gmail.com>
2016-10-06 16:22:29 -0400

    save the hash of the spec, since the only non-function property will often be the babel-generated field "displayName"

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict";
 var React = require('react');
 var Map = require("collections/map");
+var hash = require('object-hash');
 
 var originalCreateClassFunction = null;
 
@@ -15,7 +16,7 @@ StateSaver.prototype.hook = function() {
       var originalGetInitialStateFunction = spec.getInitialState;
       spec.getInitialState = function() {
         var state = originalGetInitialStateFunction.apply(this, arguments);
-        stateSaverThis.map.set({spec: spec, props: this.props}, state);
+        stateSaverThis.map.set({spec: spec, specHash: hash(spec), props: this.props}, state);
         return state;
       };
       return originalCreateClassFunction.apply(React, arguments);
