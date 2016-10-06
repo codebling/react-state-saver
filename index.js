f34aa32e221fdeec1712399e4df2b9452e21563b
Code Bling <codeblingx@gmail.com>
2016-10-06 13:51:29 -0400

    export hook and unhook as functions instead of impolitely running on module load

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 "use strict";
 var React = require('react');
 
+var originalCreateClassFunction = null;
 
-var createClass = React.createClass;
-React.createClass = function(spec) {
-  console.log('creating class!');
-  var originalGetInitialState = spec.getInitialState;
-  spec.getInitialState = function() {
-    console.log('getting state!!');
-    return originalGetInitialState.apply(this, arguments);
-  };
-  return createClass.apply(React, arguments);
-};
+function hook() {
+  if(originalCreateClassFunction === null) {
+    originalCreateClassFunction = React.createClass;
+    React.createClass = function(spec) {
+      console.log('bla');
+      var originalGetInitialStateFunction = spec.getInitialState;
+      spec.getInitialState = function() {
+        return originalGetInitialStateFunction.apply(this, arguments);
+      };
+      return originalCreateClassFunction.apply(React, arguments);
+    };
+  }
+}
+
+function unhook() {
+  if(originalCreateClassFunction !== null) {
+    React.createClass = originalCreateClassFunction;
+    originalCreateClassFunction = null;
+  }
+}
+
+module.exports.hook = hook;
+module.exports.unhook = unhook;
