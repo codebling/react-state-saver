89d9f70e37225526703fc185fd858bc85d7f7b1d
Code Bling <codeblingx@gmail.com>
2016-10-06 01:18:51 -0400

    implemented basic hooking functionality

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 "use strict";
+var React = require('react');
 
+
+var createClass = React.createClass;
+React.createClass = function(spec) {
+  console.log('creating class!');
+  var originalGetInitialState = spec.getInitialState;
+  spec.getInitialState = function() {
+    console.log('getting state!!');
+    return originalGetInitialState.apply(this, arguments);
+  };
+  return createClass.apply(React, arguments);
+};
