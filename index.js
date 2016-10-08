f5a32a844a33abc7cb20f677a7c97e69e3a582e6
Code Bling <codeblingx@gmail.com>
2016-10-08 17:43:58 -0400

    override 'require' keyword to change cached classes as well

--- index.js
+++ index.js
@@ -1,25 +1,43 @@
 "use strict";
+var Module = require('module');
 var React = require('react');
 var hash = require('object-hash');
 
 var originalCreateClassFunction = null;
+var originalRequire = null;
+var markedProperty = '__MARKED_FOR_REACT_STATE_SAVING__';
+var originalGetInitialStateFunctionSavingProperty = '__ORIGINAL_GET_INITIAL_STATE_FUNCTION__';
 
 var StateSaver = function(data) {
   this.map = new Map(data);
 };
 StateSaver.prototype.installCaptureHook = function() {
-  if(originalCreateClassFunction === null) {
+  if(originalRequire === null) {
     originalCreateClassFunction = React.createClass;
     var stateSaver = this;
-    React.createClass = function(spec) {
-      var originalGetInitialStateFunction = spec.getInitialState;
-      spec.getInitialState = function() {
-        var state = originalGetInitialStateFunction.apply(this, arguments);
+    var getReplacementGetInitiailStateFunction = function(spec) {
+      return function() {
+        var state = spec[originalGetInitialStateFunctionSavingProperty].apply(this, arguments);
         stateSaver.map.set({specHash: hash(spec), propsHash: hash(this.props)}, state);
         return state;
-      };
-      return originalCreateClassFunction.apply(React, arguments);
+      }
     };
+    React.createClass = function(spec) {
+      spec[originalGetInitialStateFunctionSavingProperty] = spec.getInitialState;
+      spec.getInitialState = getReplacementGetInitiailStateFunction(spec);
+      var reactClass = originalCreateClassFunction.apply(React, arguments);
+      reactClass[markedProperty] = true;
+      return reactClass;
+    };
+    originalRequire = Module.prototype.require;
+    Module.prototype.require = function() {
+      var potentialReactClass = originalRequire.apply(this, arguments);
+      if(potentialReactClass[markedProperty]) {
+        var spec = potentialReactClass.prototype;
+        spec.getInitialState = getReplacementGetInitiailStateFunction(spec);
+      }
+      return potentialReactClass;
+    }
   }
 };
 StateSaver.prototype.installReplayHook = function() {
@@ -36,9 +54,11 @@ StateSaver.prototype.installReplayHook = function() {
   }
 };
 StateSaver.prototype.uninstallCaptureHook = function() {
-  if(originalCreateClassFunction !== null) {
+  if(originalRequire !== null) {
     React.createClass = originalCreateClassFunction;
     originalCreateClassFunction = null;
+    Module.prototype.require = originalRequire;
+    originalRequire = null;
   }
 };
 StateSaver.prototype.getSavedState = function() {
